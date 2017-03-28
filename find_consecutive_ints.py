9b35530ec41b8cf3d32457ed8bcf42733c56b7ce
avidness <adavidness@gmail.com>
2017-03-27 20:59:28 -0400

    Fix for edge cases, added more tests

--- find_consecutive_ints.py
+++ find_consecutive_ints.py
@@ -15,9 +15,10 @@ def is_consecutive(sub_array):
 
 # Check each sub_array of size seq_len
 def find_consecutive_runs(in_array):
+	if in_array == None: return None
 	seq_len = 3
-	out_array = []	
-	for i in range(0, len(in_array)-seq_len):
+	out_array = []
+	for i in range(0, len(in_array)-seq_len+1):
 		if is_consecutive( in_array[i:i+seq_len] ):
 			out_array.append(i)
 	return out_array if len(out_array) > 0 else None
@@ -27,10 +28,13 @@ def run_test():
 	assert (find_consecutive_runs([3,2,1]) == [0])
 	assert (find_consecutive_runs(None) == None)
 	assert (find_consecutive_runs([0]) == None)
-	assert (find_consecutive_runs() == None)
+	assert (find_consecutive_runs([1,2,1]) == None)
+	assert (find_consecutive_runs([-1,0,1]) == [0])
+	assert (find_consecutive_runs([-1,-2,-3]) == [0])
+	assert (find_consecutive_runs([2,5,7,2,3,4]) == [3])
+	assert (find_consecutive_runs([3,4,6,8,3,1]) == None)
+	assert (find_consecutive_runs([1,2,3,4,5,6,7]) == [0,1,2,3,4])
+	assert (find_consecutive_runs([1,2,3,5,10,9,8,9,10,11,7,8,7]) == [0,4,6,7])
 
-in_array = [1, 2, 3, 5, 10, 9, 8, 9, 10, 11, 7, 8, 7]
-print find_consecutive_runs(in_array)
-print find_consecutive_runs([1,2,3,4])
-print find_consecutive_runs([1,2,3])
 run_test()
+print "Passed all tests"
